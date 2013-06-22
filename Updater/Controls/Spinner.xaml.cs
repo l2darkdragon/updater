8d89b4b0a200e3ef5a1a05ae80ba34fef570fb02
Zdenek Rindt <zdenek.rindt@gmail.com>
2013-06-22 14:28:50 +0200

    Debug message cleanup.

--- Updater/Controls/Spinner.xaml.cs
+++ Updater/Controls/Spinner.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Updater.Controls
 {
@@ -24,7 +12,7 @@ namespace Updater.Controls
         /// <summary>
         /// Dependancy property for IsSpinning property.
         /// </summary>
-        public static DependencyProperty IsSpinningProperty = DependencyProperty.Register("IsSpinning", typeof(Boolean), typeof(Spinner), new PropertyMetadata(true, new PropertyChangedCallback(IsSpinningChanged)));
+        public static DependencyProperty IsSpinningProperty = DependencyProperty.Register("IsSpinning", typeof(Boolean), typeof(Spinner));
 
         /// <summary>
         /// Gets or sets value indicating whether the spinner is spinning.
@@ -47,11 +35,6 @@ namespace Updater.Controls
         public Spinner()
         {
             InitializeComponent();
-        }
-
-        private static void IsSpinningChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
- 	        System.Diagnostics.Debug.WriteLine("Property " + e.Property + " has changed its value from " + e.OldValue + " to " + e.NewValue);
         }
 
     }
